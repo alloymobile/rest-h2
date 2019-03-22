4fa67dbd1dc03227b7fed5db0bf38578da0b00d4
alloymobile <alloymobiletech@gmail.com>
2019-03-22 09:00:29 -0400

    Basic setup

---
+++ h2/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+/*
+INSERT into person(id,name,location,birth_date)
+  values(100001,'Tapas','Toronto',sysdate());
+
+INSERT into person(id,name,location,birth_date)
+  values(100002,'James','New York',sysdate());
+
+INSERT into person(id,name,location,birth_date)
+  values(100003,'Peter','London',sysdate());
+*/
