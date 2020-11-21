920a6bddc7b7b03f707e4e2432b740f8d4cb6a55
alloymobile <alloymobileinc@gmail.com>
2020-11-21 10:05:17 -0500

    YL00-1000#Update junit

--- h2/src/test/java/com/rbc/h2/H2ApplicationTests.java
+++ h2/src/test/java/com/rbc/h2/H2ApplicationTests.java
@@ -1,16 +1,35 @@
 package com.rbc.h2;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class H2ApplicationTests {
 
+    @LocalServerPort
+    int randomServerPort;
+
     @Test
-    public void contextLoads() {
+    public void testGetDepartmentListSuccess() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        final String baseUrl = "http://localhost:" + randomServerPort + "/department";
+        URI uri = new URI(baseUrl);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(true, result.getBody().contains("name"));
     }
 
 }
