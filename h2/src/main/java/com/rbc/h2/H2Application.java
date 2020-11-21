bea43b4fd209e7a6f6e55bd1185e108f08eebe8f
Tapas Mandal <tapas@alloymobile.ca>
2020-11-21 17:40:29 -0500

    YL00-1000#Update jar to war

--- h2/src/main/java/com/rbc/h2/H2Application.java
+++ h2/src/main/java/com/rbc/h2/H2Application.java
@@ -2,9 +2,17 @@ package com.rbc.h2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class H2Application {
+public class H2Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(H2Application.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(H2Application.class, args);
